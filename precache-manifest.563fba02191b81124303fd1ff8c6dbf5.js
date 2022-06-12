c609a55c729a84ff39e3475d896c438bec277e59
Anonymous <100111685+user-anonymous-25@users.noreply.github.com>
2022-06-12 14:45:32 +0545

    Updates

---
+++ precache-manifest.563fba02191b81124303fd1ff8c6dbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc05dc1a759637b2b86e",
+    "url": "/TextUtils-app/static/css/main.c69c459c.chunk.css"
+  },
+  {
+    "revision": "cc05dc1a759637b2b86e",
+    "url": "/TextUtils-app/static/js/main.cc05dc1a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1af1368c091066cd9791",
+    "url": "/TextUtils-app/static/js/2.1af1368c.chunk.js"
+  },
+  {
+    "revision": "d9964dae577db6ae6a4a",
+    "url": "/TextUtils-app/static/js/runtime~main.d9964dae.js"
+  },
+  {
+    "revision": "eeb4ba03c3440a2b5be7be9175142c65",
+    "url": "/TextUtils-app/index.html"
+  }
+];
